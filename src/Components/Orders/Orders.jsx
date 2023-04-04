7c00cce0bdd060176e4851840d19e0b661eab103
Sanjidul-Alom-Shifat <sanjidul.alom.shifat.official@gmail.com>
2023-04-05 00:16:00 +0600

    adding react router dom

---
+++ src/Components/Orders/Orders.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Cart from '../Cart/Cart';
+
+const Orders = () => {
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h2>oders layout</h2>
+            </div>
+            <div className="cart-container">
+                <Cart cart={[]}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Orders; <h2>oders layout</h2>
